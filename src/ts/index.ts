8f80e3f5fd8e9dd5451ab21a1e0797bc72c41e4d
Ruben Maurer <rubenmaurer@live.de>
2020-04-09 23:20:06 +0200

    Fix multiple issues with NaN

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,81 +1,94 @@
-import { Howl, Howler } from 'howler';
+import { Howl } from 'howler';
 
-let time = 25;
-let interval: number;
+class Bolognese {
+    alarm: any;
+    timer: Element;
+    time: number;
+    interval: number;
 
-var counter: number;
-var display: number;
-var sub_dis: number;
-var is_ticking: boolean;
+    counter: number;
+    display: number;
+    sub_dis: number;
+    is_ticking: boolean;
 
-var sound = new Howl({
-    volume: 0.5,
-    html5: true,
-    src: ['sounds/owl.mp3']
-});
+    constructor(time: number) {
+        this.timer = document.querySelector("#timer");
 
-const timer = document.querySelector("#timer");
+        document.querySelector("#pomodoro-button").addEventListener("click", () => this.updateTimer(25));
+        document.querySelector("#l-break-button").addEventListener("click", () => this.updateTimer(20));
+        document.querySelector("#s-break-button").addEventListener("click", () => this.updateTimer(5));
 
-document.querySelector("#pomodoro-button").addEventListener("click", () => updateTimer(25));
-document.querySelector("#l-break-button").addEventListener("click", () => updateTimer(20));
-document.querySelector("#s-break-button").addEventListener("click", () => updateTimer(5));
+        document.querySelector("#start").addEventListener("click", () => this.startTimer());
+        document.querySelector("#stop").addEventListener("click", () => this.stopTimer());
+        document.querySelector("#reset").addEventListener("click", () => this.resetTimer());
 
-document.querySelector("#start").addEventListener("click", () => startTimer());
-document.querySelector("#stop").addEventListener("click", () => stopTimer());
-document.querySelector("#reset").addEventListener("click", () => resetTimer());
+        this.alarm = new Howl({
+            volume: 0.5,
+            html5: true,
+            src: ['sounds/owl.mp3']
+        });
 
-function initTimer() {
-    counter = time * 60;
-    display = counter / 60;
-    sub_dis = 60;
-    is_ticking = false;
+        this.time = time;
+        this.initTimer();
+    }
+
+    initTimer() {
+        this.counter = this.time * 60;
+        this.display = this.time;
+        this.sub_dis = 0;
+        this.is_ticking = false;
+    }
+    
+    startTimer() { 
+        let pre: string;
+        let sub: string;
+        
+        if (!this.is_ticking) {
+            this.is_ticking = true;
+            this.interval = setInterval(() => {
+                this.counter = this.counter - 1;
+                this.display = Math.floor(this.counter / 60);
+
+                if (this.sub_dis == 0) this.sub_dis = 60;
+                this.sub_dis--;
+                
+                pre = this.display.toString();
+                sub = this.sub_dis.toString();
+                if (this.sub_dis < 10) sub = "0" + this.sub_dis;
+                if (this.display < 10) pre = 0 + this.display.toString();
+    
+                this.timer.textContent = pre + ":" + sub;
+                if(this.counter === 0) {
+                    clearInterval(this.interval);
+    
+                    this.alarm.play();
+                    this.is_ticking = false;
+                }
+            }, 1000)
+        };
+    }
+    
+    stopTimer() {
+        this.is_ticking = false;
+        clearInterval(this.interval);
+    }
+    
+    resetTimer() {
+        this.stopTimer();
+        this.updateTimer(this.time);
+    }
+    
+    updateTimer(t: number) {
+        this.stopTimer();
+    
+        this.time = t;
+    
+        var value = t.toString();
+        if (t < 10) value = 0 + value;
+        this.timer.textContent = value + ":" + "00"
+    
+        this.initTimer();
+    }
 }
 
-function startTimer() {    
-    if (!is_ticking) {
-        let sub = "00";
-        let pre = time.toString();
-
-        is_ticking = true;
-        interval = setInterval(() => {
-            counter = counter - 1;
-            display = counter / 60;
-            if (sub_dis == 0) sub_dis = 60;
-            sub_dis--;
-            
-            sub = sub_dis.toString();
-            if (sub_dis < 10) sub = "0" + sub_dis;
-            if (display < 10) pre = 0 + Math.floor(display).toString();
-
-            timer.textContent = pre + ":" + sub;
-            if(counter === 0) {
-                clearInterval(interval);
-
-                sound.play();
-                is_ticking = false;
-            }
-        }, 1000)
-    };
-}
-
-function stopTimer() {
-    is_ticking = false;
-    clearInterval(interval);
-}
-
-function resetTimer() {
-    stopTimer();
-    updateTimer(time);
-}
-
-function updateTimer(t: number) {
-    stopTimer();
-
-    time = t;
-
-    var value = t.toString();
-    if (t < 10) value = 0 + value;
-    timer.textContent = value + ":" + "00"
-
-    initTimer();
-}
+new Bolognese(25);
